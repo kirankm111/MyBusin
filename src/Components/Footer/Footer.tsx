ed8c9d2c699e3e85d7dac55b8b41de4644886938
kirankm111 <110974093+kirankm111@users.noreply.github.com>
2023-07-15 15:55:44 +0530

    Update Footer.tsx

---
+++ src/Components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../App.css';
+
+function Footer() {
+  return (
+      <div className="sitefooter">  
+              @Copyright SriLakshmi Narayana Uma Small Scale Industry 2022- All Right Reserved.   
+             </div> 
+  );
+}
+
+export default Footer;
